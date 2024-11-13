5bf6ac4d3b049228345f2c40e0eb35c9c8f98774
Jigneshkalal <jigneshkalal67@gmail.com>
2024-11-13 11:23:59 +0000

    Variables and datatypes

---
+++ BasicJavaScript/Datatypes.js
@@ -0,0 +1,6 @@
+let name="jignesh"
+let age= 18
+let booleans=true 
+let Null= Empty-value // Empty value in any varable //
+
+// this all are datatypes 
---
+++ BasicJavaScript/Variable.js
@@ -0,0 +1,4 @@
+const accountId= 11445
+let accountEmail= "JIgnesh@gmail.accountEmail"
+var accountpass =1234
+ // Always use let variable because it only redefine in block of scope //
