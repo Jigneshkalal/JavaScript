23852828bb53773e936f798088e48bce10e75962
Jigneshkalal <jigneshkalal67@gmail.com>
2024-11-15 08:39:16 +0000

    Comparision

---
+++ BasicJavaScript/03_DatatypeConversion.js
@@ -0,0 +1,20 @@
+/* let logdinn="1"
+
+let newlogdinn=Boolean(logdinn)    
+console.log(newlogdinn); */
+
+// ******************************************Opretions*******************************************************
+
+
+let value =3
+negvalue=-value
+// console.log(negvalue)
+
+let str1="jignesh"
+let str2=" Kalal"
+str3=str1+str2
+//console.log(str3);
+
+//console.log(1+2+"3")  it's print 33
+//console.log("1"+2+3) it's print 123
+
---
+++ BasicJavaScript/04_Comparisionofoprators.js
@@ -0,0 +1,5 @@
+//console.log (null >= 0);
+
+//  if we do compare the null  vlaue to the 0  just like (null >= 0) in comparision operator 
+//so the null value is also 0 and it shows a "true"  
+
