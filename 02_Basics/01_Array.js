4a2b2dce30c0d0d19df5ffa69874558536f0e817
Jigneshkalal <jigneshkalal67@gmail.com>
2024-11-17 20:05:10 +0000

    Objsect

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,26 @@
+//const myArr=["myArr",2,3,4,5]
+/* const myarray2= new Array("Lucky",'mustafa','jignesh','mehul','vishal') */
+
+//myarray2.push('Bhau')  it is used to add any other element in array 
+//myarray2.pop()       it is used for remove any last element 
+
+
+/* const newarrary= myarray2.join()  .join is used to convert Aeer to string
+console.log(myarray2);
+console.log(typeof newarrary); */
+
+const Marvel =["ironman","thor","Hulk"]
+const Dc =["Superman","Batman","flash"]
+
+/* Marvel.push(Dc) we can't merg arry using push because it push whole arry to another array */
+/* const allmarval=Marvel.concat(Dc) how to merge Arry with using concat method*/  
+/* const allnewheros=[...Marvel, ...Dc]   New method Spread operetor
+ */
+/* const anotehrarray=[1,2,3,[4,5,6,[7,8,9,[10,11,12]]]]
+const newanotherarray=anotehrarray.flat(Infinity)  .flat method used to get a single merge arry from the array in array   */
+
+console.log( );
+
+
+
+
