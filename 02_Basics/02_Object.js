c11ce7545dfcb26ad6d09ec33719f9a695e62a68
Jigneshkalal <jigneshkalal67@gmail.com>
2024-11-17 20:50:49 +0000

    Object with function

---
+++ 02_Basics/02_Object.js
@@ -0,0 +1,24 @@
+const object ={
+    name:"jignesh",
+    age:20,
+    email:"Jignesh@gmail.com"
+}
+/* console.log(object.name);
+console.log(object["email"]); */
+
+object.age=22 
+//console.log(object.age); if we neec to change the value in object so just access the key and overritw the value
+
+/* object.test=function() {
+    console.log("Hellow javascript");   Object with function 
+} */
+
+
+/* object.test2=function() {
+    console.log(`Hii my name is ${this.name}`);   we access the key of object in function just using "this"  the "this" is define current object   
+} */
+
+/* console.log(object.test());
+console.log(object.test2()); */
+
+
