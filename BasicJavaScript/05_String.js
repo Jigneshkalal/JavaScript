ff13467c05283e10304ede8645c66c7850034618
Jigneshkalal <jigneshkalal67@gmail.com>
2024-11-17 17:42:28 +0000

    02_ Basics

---
+++ BasicJavaScript/05_String.js
@@ -0,0 +1,20 @@
+let Firtstname= "jignesh"
+let lastname="kalal"
+
+let fullname= Firtstname+lastname 
+
+//console.log(fullname)
+
+//console.log(`Hellow myname is ${Firtstname} and my last name is ${lastname}`);
+let gamename="jignesh kalal"
+
+//console.log(gamename.length);
+//console.log(gamename.toLowerCase);
+
+const newstring=gamename.substring(0,5)
+//console.log(newstring);
+const anotherstring=gamename.slice(0,5)
+console.log(anotherstring);
+
+
+
