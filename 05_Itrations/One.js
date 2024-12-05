f76ea19c59a8f1535bc8fd69863b9681ff6a8b3a
Jigneshkalal <jigneshkalal67@gmail.com>
2024-12-05 08:54:59 +0000

    Loops

---
+++ 05_Itrations/One.js
@@ -0,0 +1,57 @@
+// for Loop
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);     Basic for loop
+    
+    
+// } 
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element==5) {
+//         console.log( 5 ,"is the bset number");
+        
+//     }
+//     console.log(element);
+    
+// } 
+
+
+// for (let i = 0; i <=10; i++) {
+//     for (let j = 0; j <=10; j++) {                                               Nested loop
+//         console.log(`Outer loop ${i} and inner loop is ${j}`);
+         
+//     }    
+// }
+const myarry=["superman","batmsan","flash"]
+
+// for (let i = 0; i < myarry.length; i++) {
+//     const element =myarry[i];   array with for loop 
+//     console.log(element);
+    
+    
+// }
+
+
+//   BReak and continue 
+
+// for (let  i= 1;  i<=10; i++) {
+//     if (i==5) {
+//         console.log(`Detected ${i}`);                                            Break 
+//         break;
+        
+//     }
+//     console.log(i);
+    
+//}
+
+/* for (let  i= 1;  i<=10; i++) {
+    if (i==5) {
+        console.log(`Detected ${i}`);                                       Continue
+        continue;
+        
+    }
+    console.log(i);
+    
+} */
---
+++ 05_Itrations/Two.js
@@ -0,0 +1,25 @@
+/* let index=0
+
+while (index<=10) {
+    console.log(`values of index is ${index}`);
+    index=index+2;                                      While Loop
+} */
+
+/* let myarry=["flash","Batman","Superman"]
+let arr=0 */
+/* while (arr<myarry.length) {
+
+    console.log(`Value of my arry is ${myarry[arr]}`);
+    arr=arr+1
+
+} */
+
+
+    /* let score=1
+
+ do {
+    console.log(`score is ${score} `);
+    score++                                 Do while Loop
+    
+} while (score<=10); */
+
