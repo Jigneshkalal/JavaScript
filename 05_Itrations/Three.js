fc85ee1b0a0d7a4633246eeaa317acd08e50e1bc
Jigneshkalal <jigneshkalal67@gmail.com>
2024-12-05 08:55:34 +0000

    For Of and For each loop

---
+++ 05_Itrations/Three.js
@@ -0,0 +1,28 @@
+//for of Loop
+
+/* const array=[1,2,3,4,5,6]
+
+for (const Numofarry of array) {
+    console.log(Numofarry);                                              For Of Loop 
+    
+} */
+
+/*     let greeting="Hii Good Morning"
+
+    for (const greet of greeting) {
+        console.log(greet);
+        
+    } */
+
+
+    /*  const  coding=["js","Javascript","CPP","Python"]
+     coding.forEach(function(Valueofarry) {
+        console.log(Valueofarry);                                         ForEach loop 
+        
+     }) */
+
+     /*    const  coding=["js","Javascript","CPP","Python"]
+        coding.forEach((Valueofarry)=> {
+           console.log(Valueofarry);                                        With arrow function without function name                   
+           
+        })  */
